5094fb1a7aba319481aa40b56983bbde27cc0a9e
Tyler Blaszak <tab890@yahoo.com>
2021-12-03 21:58:09 -0500

    feat(frontend): initial frontend setup with vue3, ts, and d3

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+import D3Dashboard from "./components/D3Dashboard.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <D3Dashboard />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Options, Vue } from "vue-class-component";
+import D3Dashboard from "../components/D3Dashboard.vue"; // @ is an alias to /src
+
+// @Options({
+// components: {
+// D3Dashboard,
+// },
+// })
+// export default class Home extends Vue {}
+</script>
